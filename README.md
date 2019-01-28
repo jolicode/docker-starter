46ab56c9ca898a7f019ce32923afd65ac1dc6ac0
Loïck Piera <pyrech@gmail.com>
2019-01-28 20:30:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# JoliCode's Docker starter kit
+
+## Before using the stack (remove this chapter once done)
+
+After copy/pasting this starter kit to your project and before first launch
+you will need to (in this order):
+    - find and replace all mentions of myapp.joli with the domain of your choice
+    - find and replace all mentions of MYAPP_UID with the constant name of your choice
+    - find and replace all mentions of myapp with the name of the project
+    
+>*Note*: The name of your project will be used as a prefix for docker container
+> names, as the user inside the container, the password of your root database user
+> and many other small things.
+
+Generate the SSL certificate to use in the local stack:
+
+```bash
+cd infrastructure/development/services/router
+./generate-ssl.sh
+cd -
+```
+
+You are ready to go!
+
+>*Note*: Some Fabric tasks have been added for DX purposes. Checkout and adapt
+> the tasks `install`, `migrate` and `cache_clear` to your project
+    
+## Running the app locally
+
+### Requirements
+
+A docker environment is provided and requires you to have these tools available:
+
+ * docker
+ * docker-compose
+ * fabric
+
+Install and run `pipenv` to install the required tools:
+
+```bash
+pipenv install
+```
+
+You can configure your current shell to be able to use fabric commands directly
+(without having to prefix everything by `pipenv run`)
+
+```bash
+pipenv shell
+```
+
+### Domain configuration (first time only)
+
+Before running the app for the first time, ensure the domain name `myapp.joli`
+point the IP of your Docker deamon by editing your `/etc/hosts` file.
+
+This IP is probably 127.0.0.1 unless you run Docker in a special VM (docker-machine, dinghy, etc).
+
+```
+echo '127.0.0.1 myapp.joli' | sudo tee -a /etc/hosts
+```
+
+Using dinghy? Run `dinghy ip` to get the IP of the VM.
+
+### Starting the stack
+
+Launch the stack by running this command:
+
+```bash
+fab start
+```
+
+> Note: the first start of the stack should take a few minutes.
+
+The site is now accessible at [https://myapp.joli](https://myapp.joli)
+(you may need to accept self-signed SSL certificate).
+
+### Running tests (unit & functional)
+
+```bash
+fab tests
+```
+
+### Builder
+
+Having some composer, yarn or another modifications to make on the project?
+Start the builder which will give you access to a container with all these
+tools available:
+
+```bash
+fab builder
+```
+
+### Other tasks
+
+Checkout `fab -l` to have the list of available fabric tasks.
---
+++ fabfile.py
@@ -0,0 +1,138 @@
+from fabric.api import task, env
+from fabric.operations import local, _shell_escape, settings
+from fabric.context_managers import quiet
+import os
+import re
+from sys import platform
+
+
+@task
+def start():
+    """
+    Be sure that everything is started and installed
+    """
+    if env.dinghy:
+        machine_running = local('dinghy status', capture=True)
+        if machine_running.splitlines()[0].strip() != 'VM: running':
+            local('dinghy up --no-proxy')
+            local('docker-machine ssh dinghy "echo \'nameserver 8.8.8.8\' | sudo tee -a /etc/resolv.conf && sudo /etc/init.d/docker restart"')
+
+    up()
+    cache_clear()
+    install()
+    migrate()
+
+
+@task
+def up():
+    """
+    Ensure infrastructure is sync and running
+    """
+    docker_compose('build')
+    docker_compose('up --remove-orphans -d')
+
+
+@task
+def stop():
+    """
+    Stop the infrastructure
+    """
+    docker_compose('stop')
+
+
+@task
+def logs():
+    """
+    Show logs of infrastructure
+    """
+    docker_compose('logs -f --tail=150')
+
+
+@task
+def install():
+    """
+    Install frontend application (composer, yarn, assets)
+    """
+    #docker_compose_run('composer install -n --prefer-dist', 'builder', 'myapp')
+    #docker_compose_run('yarn', 'builder', 'myapp')
+
+
+@task
+def cache_clear():
+    """
+    Clear cache of the frontend application
+    """
+    #docker_compose_run('rm -rf var/cache/', 'builder', 'myapp', no_deps=True)
+
+
+@task
+def migrate():
+    """
+    Migrate database schema
+    """
+    #docker_compose_run('php bin/console doctrine:database:create --if-not-exists', 'builder', 'myapp', no_deps=True)
+    #docker_compose_run('php bin/console doctrine:migration:migrate -n', 'builder', 'myapp', no_deps=True)
+
+
+@task
+def ssh():
+    """
+    Ssh into frontend container
+    """
+    docker_compose('exec --user=myapp --index=1 frontend /bin/bash')
+
+
+@task
+def builder():
+    """
+    Bash into a builder container
+    """
+    docker_compose_run('bash', 'builder', 'myapp')
+
+
+def docker_compose(command_name):
+    local('MYAPP_UID=%s docker-compose -p myapp %s %s' % (
+        env.uid,
+        ' '.join('-f infrastructure/development/' + file for file in env.compose_files),
+        command_name
+    ))
+
+
+def docker_compose_run(command_name, service, user="myapp", no_deps=False):
+    args = [
+        'run '
+        '--rm '
+        '-u %s ' % _shell_escape(user)
+    ]
+
+    if no_deps:
+        args.append('--no-deps ')
+
+    docker_compose('%s %s /bin/bash -c "%s"' % (
+        ' '.join(args),
+        _shell_escape(service),
+        _shell_escape(command_name)
+    ))
+
+
+def set_local_configuration():
+    env.compose_files = ['docker-compose.yml']
+    env.uid = int(local('id -u', capture=True))
+    env.root_dir = os.path.dirname(os.path.abspath(__file__))
+
+    if env.uid > 256000:
+        env.uid = 1000
+
+    with quiet():
+        try:
+            docker_kernel = "%s" % local('docker version --format "{{.Server.KernelVersion}}"', capture=True)
+        except:
+            docker_kernel = ''
+
+    if platform == "linux" or platform == "linux2" or docker_kernel.endswith('linuxkit-aufs'):
+        env.dinghy = False
+    elif platform == "darwin":
+        env.dinghy = True
+
+
+set_local_configuration()
