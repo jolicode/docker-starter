b28b9ce1d31c726aff5f71f8182317eda4be71fa
Loïck Piera <pyrech@gmail.com>
2019-01-29 22:17:53 +0100

    Massive refacto to ease usages (thanks @lyrixx 😘)

--- infrastructure/docker/services/router/generate-ssl.sh
+++ infrastructure/docker/services/router/generate-ssl.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-openssl req -x509 -sha256 -newkey rsa:4096 -keyout ssl/myapp.joli-key.pem -out ssl/myapp.joli-cert.pem -days 3650 -nodes -config openssl.cnf
+BASE=$(dirname $0)/
+
+mkdir $BASE/certs
+
+openssl req -x509 -sha256 -newkey rsa:4096 \
+    -keyout $BASE/certs/key.pem \
+    -out $BASE/certs/cert.pem \
+    -days 3650 -nodes -config \
+    $BASE/openssl.cnf
