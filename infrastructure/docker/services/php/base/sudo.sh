1f4b395e11c55f36c433933a6a1623143d8cc6d2
Joel Wurtz <jwurtz@jolicode.com>
2024-05-03 11:14:56 +0200

    feat(images): add possibility to have sudo

---
+++ infrastructure/docker/services/php/base/sudo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export GOSU_PLEASE_LET_ME_BE_COMPLETELY_INSECURE_I_GET_TO_KEEP_ALL_THE_PIECES="I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die."
+exec gosu 0:0 $@
