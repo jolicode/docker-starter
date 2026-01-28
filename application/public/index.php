dc503a1d494faaaa9f0b8f11ac5fbf20463e21a7
Grégoire Pineau <lyrixx@lyrixx.info>
2026-01-28 11:05:53 +0100

    Simpler APP_ENV variable management

--- application/public/index.php
+++ application/public/index.php
@@ -1,3 +1,5 @@
 <?php
 
 echo 'Hello world from PHP ', \PHP_MAJOR_VERSION, '.', \PHP_MINOR_VERSION, '.', \PHP_RELEASE_VERSION, " inside Docker! \n";
+
+echo 'Environment: ', $_SERVER['APP_ENV'] ?? 'not set', "\n";
